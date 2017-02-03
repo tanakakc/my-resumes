96ce732c4feaba9df8fd40c4886e7a5c010060fb
Keishi Tanaka <kc@hokuken.com>
2017-02-04 00:59:41 +0900

    Add WorkHistory validation spec.

--- app/models/work_history.rb
+++ app/models/work_history.rb
@@ -1,3 +1,11 @@
 class WorkHistory < ApplicationRecord
   belongs_to :user
+  validates_presence_of :company_name, :joined_at
+  validate :leaved_at_must_after_joined_at
+
+  def leaved_at_must_after_joined_at
+    if leaved_at && leaved_at <= joined_at
+      self.errors.add :leaved_at, "退職日は入社日よりも後にしてください"
+    end
+  end
 end
--- spec/models/work_history_spec.rb
+++ spec/models/work_history_spec.rb
@@ -1,11 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe WorkHistory, type: :model do
+  it "is valid with user, campany name and joined at as datetime" do
+    user = User.create(
+      email: "test@example.com",
+      password: "password", password_confirmation: "password"
+    )
+    work_history = user.work_histories.build(
+      company_name: "Example Inc.",
+      joined_at: "2010-4-1"
+    )
+    expect(work_history).to be_valid
+  end
+
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_presence_of(:company_name) }
   it { is_expected.to validate_presence_of(:joined_at) }
 
+  it "is invalid with joined_at as string" do
+    user = User.create(
+      email: "test@example.com",
+      password: "password", password_confirmation: "password"
+    )
+    work_history = user.work_histories.build(joined_at: "a")
+    work_history.valid?
+    expect(work_history.errors[:joined_at]).to include("を入力してください")
+  end
+
+  it "is invalid with joined_at as integer" do
+    user = User.create(
+      email: "test@example.com",
+      password: "password", password_confirmation: "password"
+    )
+    work_history = user.work_histories.build(joined_at: 1)
+    work_history.valid?
+    expect(work_history.errors[:joined_at]).to include("を入力してください")
+  end
+
+
   # 退職日が入社日よりも前なら無効な状態であること
-  it "is invalid leaved at is before joined at"
+  it "is invalid leaved at is before joined at" do
+    user = User.create(
+      email: "test@example.com",
+      password: "password", password_confirmation: "password"
+    )
+    work_history = user.work_histories.build(
+      company_name: "Example Inc.",
+      joined_at: "2010-4-1", leaved_at: "2009-10-31"
+    )
+    work_history.valid?
+    expect(work_history.errors[:leaved_at]).to include("退職日は入社日よりも後にしてください")
+  end
 end
