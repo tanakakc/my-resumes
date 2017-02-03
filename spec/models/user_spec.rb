4b08319e74d690a6dc9a14d234afce5947b32098
Keishi Tanaka <kc@hokuken.com>
2017-02-03 14:47:35 +0900

    Refine User spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,38 +10,8 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without a email" do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
-  end
-
-  # パスワードがなければ無効な状態であること
-  it "is invalid without a password" do
-    user = User.new(password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("を入力してください")
-  end
-
-  # パスワードとパスワード確認が同じでなければ無効な状態であること
-  it "is invalid with password confirmation is not same as password" do
-    user = User.new(password: "password", password_confirmation: "12345")
-    user.valid?
-    expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
-  end
-
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
-    User.create(
-      email: "test@example.com",
-      password: "password", password_confirmation: "password"
-    )
-    user = User.new(
-      email: "test@example.com",
-      password: "password", password_confirmation: "password"
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
-  end
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_confirmation_of(:password).on(:create) }
 end
