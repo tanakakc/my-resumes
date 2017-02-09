e44afaf5c2cb63af39e9721cb96a5ed7bbc8ed08
Keishi Tanaka <kc@hokuken.com>
2017-02-09 20:14:04 +0900

    Add sign in and sign out flow.

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,18 +1,49 @@
 require "rails_helper"
 
-RSpec.describe "User sign in process", type: :feature do
-  it "signs me in" do
-    User.create(
-      email: "test@example.com",
-      password: "password", password_confirmation: "password"
-    )
+feature "User sign in/out process" do
+  given(:user) { FactoryGirl.create(:user) }
 
+  scenario "Sign in with valid input" do
     visit "/users/sign_in"
-    fill_in "メールアドレス", with: "test@example.com"
-    fill_in "パスワード", with: "password"
+    expect(page).not_to have_content user.email
+    expect(page).not_to have_css("a", text: "ログアウト")
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
     click_button "ログイン"
 
-    expect(page).to have_text("test@example.com")
-    expect(page).to have_text("ログアウト")
+    expect(page).to have_content "ログインしました。"
+    expect(page).to have_content user.email
+    expect(page).to have_css("a", text: "ログアウト")
+  end
+
+  scenario "Sign in with invalid input" do
+    visit "/users/sign_in"
+
+    fill_in "メールアドレス", with: "other@exapmle.com"
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_content "メールアドレスまたはパスワードが違います。"
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "incorrect"
+    click_button "ログイン"
+
+    expect(page).to have_content "メールアドレスまたはパスワードが違います。"
+  end
+
+  scenario "Sign out" do
+    sign_in user
+    visit root_path
+
+    expect(page).not_to have_content "ログイン"
+    expect(page).to have_content "ログアウト"
+
+    click_link "ログアウト"
+
+    expect(page).to have_content "ログアウトしました。"
+    expect(page).to have_css("a", text: "ログイン")
+    expect(page).not_to have_css("a", text: "ログアウト")
   end
 end
